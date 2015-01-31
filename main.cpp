02151cd1cad80ceddb219a9710f03b7649e4fe70
samueltenka <iguavadon@gmail.com>
2015-01-30 19:06:26 -0500

    Update main.cpp
    efficiency through bounds

--- main.cpp
+++ main.cpp
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include <math.h>
 
-const int n = 15;
+const int n = 2000;
 
-int cube(int x) {return x*x*x;}
-float croot(int x) {return pow(x, 1.0/3);}
-int is_int(float x) {return x==(int)x;}
+long cube(int x) {return x*x*x;}
+float croot(long x) {return pow(x, 1.0/3);}
+bool is_int(float x) {return x==(int)x;}
 
 int main(int argc, const char * argv[])
 {
-    for(int a=0; a<=n; ++a) {
-        for(int b=0; b<=a; ++b) {
+    for(int a=1; a<=n; ++a) {
+        for(int b=1; b<a; ++b) {
             int sum = cube(a)+cube(b);
-            int clim = (int)(croot(sum-cube(b)));
-            if(clim>b) {clim=b;}
-            for(int c=0; c<clim; ++c) {
-                int d3 = sum-cube(c);
+            for(int c=a-1; c>b; --c) {
+                long c3 = cube(c);
+                long d3 = sum-cube(c); if(d3>c3) {break;}
                 float d = croot(d3);
                 if(is_int(d)) {printf("%d: %d^3+%d^3=%d^3+%d^3\n", sum, a, b, c, (int)d);}
             }
